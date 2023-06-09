f8d7900a105ba5907db6fe0a5bf09c52996bcca3
Rafael Bahiense <rafaelbahiense@id.uff.br>
2023-06-09 01:20:29 -0300

    feat(init): add service descriptor for Minecraft server

--- user_data.sh
+++ user_data.sh
@@ -52,7 +52,14 @@ __UPG__
   # Init script for starting, stopping
   cat <<INIT > /etc/init.d/minecraft
 #!/bin/bash
-# Short-Description: Minecraft server
+### BEGIN INIT INFO
+# Provides:          minecraft
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start/stop minecraft server
+### END INIT INFO
 
 start() {
   echo "Starting minecraft server from /home/minecraft..."
